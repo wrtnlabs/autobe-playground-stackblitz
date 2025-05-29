cb4c34540bb6fd8186b99376c1ff0c0cb6cf78ab
Jeongho Nam <samchon.github@gmail.com>
2025-05-29 18:12:04 +0900

    Bump up and console logging

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,11 @@ const main = async (): Promise<void> => {
   const app: INestApplication = await NestFactory.create(AppModule);
   await WebSocketAdaptor.upgrade(app);
   await app.listen(443, "0.0.0.0");
+
+  console.log("");
+  console.log("Application is running");
+  console.log("");
+  console.log("  - http://127.0.0.1:443");
+  console.log("  - http://localhost:443");
 };
 main().catch(console.error);
