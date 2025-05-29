b007a6d9e041d798b60143c84b3eb5078acb7927
Jeongho Nam <samchon.github@gmail.com>
2025-05-30 01:50:25 +0900

    Bump up and change port number

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ import { WebSocketAdaptor } from "@nestia/core";
 const main = async (): Promise<void> => {
   const app: INestApplication = await NestFactory.create(AppModule);
   await WebSocketAdaptor.upgrade(app);
-  await app.listen(443, "0.0.0.0");
+  await app.listen(5_890, "0.0.0.0");
 
   console.log("");
   console.log("Application is running");
   console.log("");
-  console.log("  - http://127.0.0.1:443");
-  console.log("  - http://localhost:443");
+  console.log("  - http://127.0.0.1:5890");
+  console.log("  - http://localhost:5890");
 };
 main().catch(console.error);
